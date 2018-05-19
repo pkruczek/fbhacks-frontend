72717cea036b8e1ab3a6e312248fc72b50a6d876
Oleksandr Dubenko <odybenko@gmail.com>
2018-05-19 16:34:32 +0200

    Add integrations

--- src/components/Integrations/Integrations.js
+++ src/components/Integrations/Integrations.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Check, Plus } from "react-feather";
 import integrations from "../../mock/integrations";
 import user from "../../mock/profile";
 import classnames from "classnames";
+import "./Integrations.css";
 
 const Integration = ({ name, added }) => {
   const className = classnames("Integration", {
     "Integration--addded": added
   });
 
-  return <div className={className}>{name}</div>;
+  return (
+    <div className={className}>
+      {name}
+      <div className="Integration-icon">{added ? <Check /> : <Plus />}</div>
+    </div>
+  );
 };
 
 class Integrations extends React.Component {
@@ -19,7 +26,11 @@ class Integrations extends React.Component {
     );
 
     return (
-      <div>
+      <div className="Integrations">
+        <h2>Integrations</h2>
+        {availableIntegrations.map(integr => (
+          <Integration name={integr} key={integr} />
+        ))}
         {addedIntegrations.map(integr => (
           <Integration name={integr} added key={integr} />
         ))}
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,15 +4,16 @@ import type { User } from "../model/profile";
 import user from "../../mock/profile";
 import integrations from "../../mock/integrations";
 import ProfilePic from "../ProfilePic/ProfilePic";
+import "./Profile.css";
 
 class Profile extends React.Component {
   user: User = user;
 
   render() {
     return (
-      <div>
+      <div className="Profile">
         <ProfilePic src={user.photoUrl} big />
-        <h2>{user.name}</h2>
+        <h2 className="Profile-name">{user.name}</h2>
       </div>
     );
   }
